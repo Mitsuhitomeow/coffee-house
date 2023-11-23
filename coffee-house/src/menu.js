5d759cf439448c06764991aa32dd56b30a91aacd
Alexey Melnikov <cancem343@gmail.com>
2023-11-23 11:19:09 +0300

    feat: create title and filters buttons at the menu page.

--- coffee-house/src/menu.js
+++ coffee-house/src/menu.js
@@ -1,22 +1,5 @@
 import './menu.html'
 import './style/main.scss'
-
-import url from './scripts/database/products.json'
-
-async function fetchData() {
-  try {
-    const response = await fetch(url);
-
-    if (!response.ok) {
-      throw new Error('ошибка при загрузке')
-    }
-
-    const data = await response.json()
-    console.log(data)
-  } catch (error) {
-    console.error('failed 404', error.message)
-    throw error
-  }
-}
+import { fetchData } from './scripts/menuGetData'
 
 fetchData()
---
+++ coffee-house/src/scripts/menuGetData.js
@@ -0,0 +1,17 @@
+import url from './database/products.json'
+
+export async function fetchData() {
+  try {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error('ошибка при загрузке')
+    }
+
+    const data = await response.json()
+    console.log(data)
+  } catch (error) {
+    console.error('failed 404', error.message)
+    throw error
+  }
+}
