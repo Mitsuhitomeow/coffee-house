236e3bb24ae5c86c2f3059b9bd170b3a3c953ac8
Alexey Melnikov <cancem343@gmail.com>
2023-12-03 14:45:23 +0300

    refactor: update code the fuction show burger.

--- coffee-house/src/scripts/burger/burgerMenuApp.js
+++ coffee-house/src/scripts/burger/burgerMenuApp.js
@@ -3,16 +3,23 @@ const NAV_MENU = document.querySelector('.header__nav');
 const MENU_LINK = document.querySelector('.header__menu-link');
 const LINK = document.querySelectorAll('li');
 
-function toggleBurger() {
-  const isOpen = BTN_BURGER.classList.toggle('burger-active');
-
-  NAV_MENU.classList.toggle('open', isOpen);
-  MENU_LINK.classList.toggle('open', isOpen);
-  document.body.classList.toggle('_lock', isOpen)
-}
-
 export const btnBurgerMenu = () => {
-  BTN_BURGER.addEventListener('click', toggleBurger)
-  MENU_LINK.addEventListener('click', toggleBurger)
-  LINK.forEach(link => link.addEventListener('click', toggleBurger));
+
+  BTN_BURGER.addEventListener('click', () => {
+    NAV_MENU.classList.toggle('open');
+    MENU_LINK.classList.toggle('open')
+    document.body.classList.toggle('_lock');
+  })
+
+  MENU_LINK.addEventListener('click', () => {
+    document.body.classList.remove('_lock');
+    NAV_MENU.classList.remove('open');
+    MENU_LINK.classList.remove('open');
+  })
+
+  LINK.forEach(link => link.addEventListener('click', () => {
+    document.body.classList.remove('_lock');
+    NAV_MENU.classList.remove('open');
+    MENU_LINK.classList.remove('open');
+  }));
 }
