ddb4a2ad3c02814582e71a30b64f8e2c496b92b0
Alexey Melnikov <cancem343@gmail.com>
2023-12-03 18:17:32 +0300

    feat: add ability to use touch swipe for the close a burger menu.

--- coffee-house/src/scripts/burger/burgerMenuApp.js
+++ coffee-house/src/scripts/burger/burgerMenuApp.js
@@ -3,6 +3,31 @@ const NAV_MENU = document.querySelector('.header__nav');
 const MENU_LINK = document.querySelector('.header__menu-link');
 const LINK = document.querySelectorAll('li');
 
+const touchCloseBurger = () => {
+  let startX = 0;
+  let endX = 0;
+
+  const minX = 100;
+
+  NAV_MENU.addEventListener('touchstart', (event) => {
+    startX = event.touches[0].clientX
+  })
+
+  NAV_MENU.addEventListener('touchmove', (event) => {
+    endX = event.touches[0].clientX
+  })
+
+  NAV_MENU.addEventListener('touchend', (event) => {
+    const distance = endX - startX
+
+    if (distance > minX) {
+      document.body.classList.remove('_lock');
+      NAV_MENU.classList.remove('open');
+      MENU_LINK.classList.remove('open');
+    }
+  })
+}
+
 export const btnBurgerMenu = () => {
 
   BTN_BURGER.addEventListener('click', () => {
@@ -22,4 +47,6 @@ export const btnBurgerMenu = () => {
     NAV_MENU.classList.remove('open');
     MENU_LINK.classList.remove('open');
   }));
+
+  touchCloseBurger()
 }
